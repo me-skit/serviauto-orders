ae47800622d6ed80e59ddf17cf2ae16740e3fbdd
Meme <meme_es@hotmail.com>
2022-11-20 18:09:37 -0600

    Update flash messages

--- resources/views/layouts/flash-message.blade.php
+++ resources/views/layouts/flash-message.blade.php
@@ -1,44 +1,44 @@
 @if ($message = Session::get('success'))
-<div class="alert alert-success alert-block">
-	<button type="button" class="close" data-dismiss="alert">×</button>
+<div class="alert alert-success alert-dismissible fade show" role="alert">
   <div class="text-center">
     {{ $message }}
   </div>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
 
 @if ($message = Session::get('error'))
-<div class="alert alert-danger alert-block">
-	<button type="button" class="close" data-dismiss="alert">×</button>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
   <div class="text-center">
     {{ $message }}
   </div>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
 
 @if ($message = Session::get('warning'))
-<div class="alert alert-warning alert-block">
-  <button type="button" class="close" data-dismiss="alert">×</button>
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
   <div class="text-center">
-  	{{ $message }}
+    {{ $message }}
   </div>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
 
 @if ($message = Session::get('info'))
-<div class="alert alert-info alert-block">
-	<button type="button" class="close" data-dismiss="alert">×</button>
+<div class="alert alert-info alert-dismissible fade show" role="alert">
   <div class="text-center">
-  	{{ $message }}
+    {{ $message }}
   </div>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
 
 @if ($errors->any())
-<div class="alert alert-danger">
-	<button type="button" class="close" data-dismiss="alert">×</button>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
   <div class="text-center">
     Por favor, verifique los errores mostrados abajo
   </div>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
